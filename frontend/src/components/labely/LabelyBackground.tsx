41dc22eba650f38645c1361798558e8870a8e6fb
Utaho Akashi <akashi.uta.1212@gmail.com>
2024-10-27 02:03:41 +0900

    add:ホーム画面へ3Dモデルを追加 #12

---
+++ frontend/src/components/labely/LabelyBackground.tsx
@@ -0,0 +1,39 @@
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import { ContactShadows, Environment, Stars } from "@react-three/drei";
+import styles from "../../css/utils/background.module.css";
+
+const LabelBackground = () => {
+  return (
+    <div className={styles.container}>
+      <Canvas
+        camera={{ position: [0, 0, 2], fov: 70 }}
+        shadows={true}
+        style={{
+          backgroundImage:
+            "linear-gradient(180deg, rgba(23, 28, 86, 1), rgba(95, 135, 250, 1))",
+        }}
+      >
+        <Environment preset="sunset" background={false} />
+        <ContactShadows
+          position={[0, -1.5, 0]}
+          opacity={0.5}
+          scale={10}
+          blur={2.5}
+          far={4.5}
+        />
+        <Stars
+          radius={10}
+          depth={100}
+          count={2000}
+          factor={10}
+          saturation={-1}
+          fade
+          speed={1}
+        />
+      </Canvas>
+    </div>
+  );
+};
+
+export default LabelBackground;
---
+++ frontend/src/components/utils/ViewCoffee.tsx
@@ -0,0 +1,53 @@
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import { Model } from "./Coffee";
+import { ContactShadows, Environment, Float } from "@react-three/drei";
+import styles from "../../css/utils/background.module.css";
+
+// Propsの型定義
+interface ViewProps {
+  enviroment:
+    | "apartment"
+    | "city"
+    | "dawn"
+    | "forest"
+    | "lobby"
+    | "night"
+    | "park"
+    | "studio"
+    | "sunset"
+    | "warehouse"
+    | undefined;
+}
+
+const ViewCoffee: React.FC<ViewProps> = ({ enviroment }) => {
+  return (
+    <div className={styles.container}>
+      <Canvas camera={{ position: [0, 0, 2], fov: 70 }} shadows={true}>
+        <Float
+          speed={6} // 回転速度。デフォルトでは1
+          rotationIntensity={0} // 回転強度。デフォルトでは1
+          floatIntensity={1} // 上下の Float の強度。デフォルトでは1
+          floatingRange={[-0.1, 0.1]} // オブジェクトが浮動する Y 軸値の範囲。デフォルトは [-0.1,0.1]
+        >
+          <Model position={[0, -3, -10]} rotation={[0, -Math.PI / 2, 0]} />
+          <directionalLight position={[3, 0, 5]} intensity={1.5} />
+          <Environment
+            preset={enviroment}
+            background
+            backgroundRotation={[0, Math.PI / -0.9, 0]}
+          />
+          <ContactShadows
+            position={[0, -1.5, 0]}
+            opacity={0.5}
+            scale={10}
+            blur={2.5}
+            far={4.5}
+          />
+        </Float>
+      </Canvas>
+    </div>
+  );
+};
+
+export default ViewCoffee;
