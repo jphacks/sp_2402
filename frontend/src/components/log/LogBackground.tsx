c79ccc57220f62816493b18be77e9c42a59af158
nanzyou <>
2024-10-28 05:02:28 +0900

    称号のところの配置を変更

---
+++ frontend/src/components/log/LogBackground.tsx
@@ -0,0 +1,10 @@
+// LogBackground.tsx
+
+import React from "react";
+import styles from "../../css/log/LogBackground.module.css";
+
+const LogBackground = () => {
+  return <div className={styles.background}></div>;
+};
+
+export default LogBackground;
